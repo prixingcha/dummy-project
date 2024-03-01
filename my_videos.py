80a79b0416278af64076fb00628bb2969862d0b6
prixingcha <pratik.np@gmail.com>
2024-03-01 16:26:31 -0500

    full subscribed list work

--- my_videos.py
+++ my_videos.py
@@ -11,18 +11,28 @@ credentials = flow.run_local_server(port=8080)
 # Create a YouTube API client
 youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 
-# Get the list of subscribed channels
-channels_response = youtube.subscriptions().list(
-    mine=True,
-    part='snippet',
-    maxResults=50  # Maximum number of results per page, change as needed
-).execute()
+next_page_token = None
+i  = 0
+while True:
+    # Get the list of subscribed channels
+    channels_response = youtube.subscriptions().list(
+        mine=True,
+        part='snippet',
+        maxResults=50,  # Maximum number of results per page, change as needed
+        pageToken=next_page_token  # Use the next page token
+    ).execute()
 
-# Iterate over the subscribed channels and print their titles
-for item in channels_response['items']:
-    snippet = item['snippet']
-    title = snippet['title']
-    print(f"Subscribed to: {title}")
+    # Iterate over the subscribed channels and print their titles
+    for item in channels_response['items']:
+        i = i  + 1
+        snippet = item['snippet']
+        title = snippet['title']
+        print(f"{i} => Subscribed to: {title}")
+
+    # Check if there are more pages
+    next_page_token = channels_response.get('nextPageToken')
+    if not next_page_token:
+        break
 
 # Shutdown the OAuth server gracefully
 # oauth_server = flow._oauth2_inst._server
