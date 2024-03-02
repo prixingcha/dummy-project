d2af3b8d696e118412c6f2c12f96349762768460
prixingcha <pratik.np@gmail.com>
2024-03-01 21:16:13 -0500

    write read subscribe file

--- get_youtube_subscribed_list.py
+++ get_youtube_subscribed_list.py
@@ -1,5 +1,6 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 import googleapiclient.discovery
+import json
 
 # OAuth 2.0 credentials flow
 flow = InstalledAppFlow.from_client_secrets_file(
@@ -11,6 +12,12 @@ credentials = flow.run_local_server(port=8080)
 # Create a YouTube API client
 youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 
+
+string_list = []
+
+all_json_obj = {}
+
+
 next_page_token = None
 i  = 0
 while True:
@@ -19,20 +26,42 @@ while True:
         mine=True,
         part='snippet',
         maxResults=50,  # Maximum number of results per page, change as needed
-        pageToken=next_page_token  # Use the next page token
+        pageToken=next_page_token,  # Use the next page token
     ).execute()
-
     # Iterate over the subscribed channels and print their titles
     for item in channels_response['items']:
         i = i  + 1
-        snippet = item['snippet']
-        title = snippet['title']
-        print(f"{i} => Subscribed to: {title}")
+        
+        # channel_name = item['snippet']['title']
+        channel_id = item['snippet']['resourceId']["channelId"]
+        
+        # title = item['snippet']['title']
+        # str = f"{channel_id} : {channel_name}"
+        
+        
+        string_list.append(channel_id)
+        
+        # all_json_obj.update(data_dict)
 
+        # print(data_dict)
+        
+        # print(json.dumps(channels_response, indent=4, ensure_ascii=True))
+        # break
+        # print(f"{title} :  https://www.youtube.com/{title}")
+    # break
     # Check if there are more pages
     next_page_token = channels_response.get('nextPageToken')
     if not next_page_token:
         break
+print('testing')
+# full_subscribe_list = json.dumps(string_list, indent=None)
+# print(string_list)
+
+print(string_list)
+# # Write the merged dictionary to a JSON file
+with open('subscribe_list.json', 'w') as file:
+    file.write(str(string_list))
+
 
 # Shutdown the OAuth server gracefully
 # oauth_server = flow._oauth2_inst._server
