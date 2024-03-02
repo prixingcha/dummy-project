b76c2f04179a73424b20aa132af5311ce9409e14
prixingcha <pratik.np@gmail.com>
2024-03-01 22:10:43 -0500

    added

--- read_subscribe_list.py
+++
@@ -1,7 +0,0 @@
-with open('subscribe_list.txt', 'r') as file:
-    data = file.read().strip()[1:-1].split(', ')
-    # data is now a list of strings containing the channel IDs
-    # print(data)
-    
-for x in data:
-    print(x)
